c9eb986c816b66797064f6e9e75105a848750500
thonydangdev <thongdang4298@gmail.com>
2022-05-06 13:23:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,526 @@
+const dataMenu = {
+    "Thời sự": {
+        sub_nav: ["Chính trị", "Dân sinh", "Giao thông", "Mekong", "Quỹ Hy vọng"],
+        colorMenu: "#b52759",
+    },
+    "Góc nhìn": {
+        sub_nav: [
+            "Bình luận nhiều",
+            "Covid 19",
+            "Chính trị& chính sách",
+            "Y tế & sức khỏe",
+            "Kinh doanh & quản trị",
+            "Giáo dục & tri thức",
+            "Môi trường",
+            "Văn hóa & lối sống",
+            "Tác giả",
+        ],
+        colorMenu: "#b52759",
+    },
+    "Thế giới": {
+        sub_nav: ["Tư liệu", "Phân tích", "Người Việt 5 châu", "Cuộc sống đó đây", "Quân sự"],
+        colorMenu: "#b52759",
+    },
+    Video: {
+        sub_nav: [
+            "Thời sự",
+            "Nhịp sống",
+            "Food",
+            "Tôi kể",
+            "Chuyện núi rừng",
+            "Pháp luật",
+            "Giải trí",
+            "Thể thao",
+            "Số hóa",
+            "Xe",
+            "Sức khỏe",
+            "Du  lịch",
+            "Thế  giới",
+            "Khoa học",
+            "Giáo dục",
+            "Tọa đàm kinh tế",
+            "Chủ đề",
+        ],
+        colorMenu: "#b52759",
+    },
+    Podcasts: {
+        sub_nav: [
+            "VnExpress hôm nay",
+            "Ly hôn",
+            "Thầm thì",
+            "Bạn ổn không?",
+            "Chuyện đi làm",
+            "Úp - Mở",
+            "Điểm tin",
+            "Họ nói gì?",
+            "Tiền làm gì?",
+            "Nguy cơ",
+            "Tôi trong gương",
+        ],
+        colorMenu: "#b52759",
+    },
+    "Kinh doanh": {
+        sub_nav: [
+            "Quốc tế",
+            "Doanh nghiệp",
+            "Chứng khoán",
+            "Bất động sản",
+            "Ebank",
+            "Vĩ mô",
+            "Tiền của tôi",
+            "Bảo hiểm",
+            "Hàng hóa",
+            "E-commerce 4.0",
+            "Vhome",
+            "eBox",
+        ],
+        colorMenu: "#065e9d",
+        contentCate: {
+            first1page: {
+                img: "https://i1-kinhdoanh.vnecdn.net/2022/05/03/gold-1651551880-2934-1651552330.jpg?w=380&h=228&q=100&dpr=1&fit=crop&s=EDPQfP8UXTxLxV9-7r_YnQ",
+                title: "Giá vàng có thể đi lên",
+                description:
+                    "Giới phân tích tiếp tục giữ quan điểm tích cực khi kim loại quý trụ ngưỡng hỗ trợ 1.900 USD mỗi ounce, tuy nhiên tâm lý của nhà đầu tư cá nhân đã dần suy yếu.",
+            },
+            newsbox: {
+                title: "Vinhomes lãi quý I gần 6.000 tỷ đồng",
+                description: ` Công ty quản lý mảng bất động sản
+                của Tập đoàn Vingroup ghi nhận doanh
+                thu hơn 8.900 tỷ đồng sau ba tháng
+                đầu năm, với lãi trước ...`,
+            },
+            footer: [
+                `Chi phí thuê và vay mua nhà ở Mỹ
+            gần bằng nhau`,
+                `Bầu Đức rót 2.000 tỷ vào trồng
+            chuối, nuôi heo trong ba tháng`,
+                `Vietravel lại lỗ hơn trăm tỷ
+            đồng`,
+            ],
+        },
+    },
+    "Khoa học": {
+        sub_nav: [
+            "Khoa học trong nước",
+            "Tin tức",
+            "Phát minh",
+            "Ứng dụng",
+            "Thế giới tự nhiên",
+            "Thường thức",
+            "Sáng kiến Khoa học",
+        ],
+        colorMenu: "#ad9634",
+        contentCate: {
+            first1page: {
+                img: "https://i1-vnexpress.vnecdn.net/2022/05/03/Tenluatructhang-1651561174-1922-1651561913.jpg?w=380&h=228&q=100&dpr=1&fit=crop&s=DC9z_D-gEI8N-hbRqXwDrQ",
+                title: "Công ty Mỹ dùng trực thăng tóm tên lửa giữa không trung",
+                description:
+                    "Sau khi phóng vệ tinh lên quỹ đạo, tầng thứ nhất của tên lửa Electron rơi xuống Thái Bình Dương, giảm tốc bằng ... ",
+            },
+            newsbox: {
+                title: "Enzyme phân hủy rác thải nhựa trong 24 giờ",
+                description: `MỸ - Nhóm nghiên cứu tại Đại học Texas phát triển enzyme phân hủy nhựa PET hiệu quả ở nhiệt độ 30 - 50 độ C và nhiều mức pH khác nhau. `,
+            },
+            footer: [
+                `Nhật thực một phần nhìn từ vũ trụ`,
+                `Máy duỗi tóc hoạt động như thế nào?`,
+                `Mũ bảo hiểm có thể nghe gọi, bật cảnh báo va chạm`,
+            ],
+        },
+    },
+    "Giải trí": {
+        sub_nav: [
+            "Giới sao",
+            "Video",
+            "Phim",
+            "Nhạc",
+            "Thời trang",
+            "Làm đẹp",
+            "Sách",
+            "Sân khấu - Mỹ thuật",
+        ],
+        colorMenu: "#b52759",
+        contentCate: {
+            first1page: {
+                img: "https://i1-giaitri.vnecdn.net/2022/05/02/zyro-image-1651485258-7747-1651486272.png?w=380&h=228&q=100&dpr=1&fit=crop&s=VRtt6ZaaW2SUjCJU4y3pSA",
+                title: "Phong độ Lee Byung Hun tuổi 52",
+                description:
+                    "Lee Byung Hun, 52 tuổi, tạo khác biệt ở showbiz Hàn nhờ gương mặt góc cạnh, nam tính. ",
+            },
+            newsbox: {
+                title: "Vợ Brooklyn Beckham vào nhóm sao mặc đẹp ở Met Gala",
+                description: `Nicola Peltz - vợ Brooklyn Beckham - được Vogue xếp vào nhóm sao mặc đẹp ở Met Gala 2022. `,
+            },
+            footer: [
+                `Elon Musk tạo dáng trên thảm đỏ Met Gala <div class="box-comment">
+            <i class="fas fa-comment-alt"></i>
+            <span>56</span>
+        </div>`,
+                `Johnny Depp - Amber Heard: Bi hài khi ra tòa <div class="box-comment">
+        <i class="fas fa-comment-alt"></i>
+        <span>56</span>
+    </div>`,
+                `Vợ chồng Brooklyn Beckham chưng diện ở Met Gala`,
+            ],
+        },
+    },
+    "Thể thao": {
+        sub_nav: [
+            "Bóng đá",
+            "Lịch thi đấu",
+            "Tennis",
+            "VM 2022",
+            "Fun Run",
+            "Bundesliga",
+            "Các môn khác",
+            "SEA Games 31",
+            "V-League",
+            "Hậu trường",
+            "Video",
+            "Ảnh",
+            "V-Race",
+            "Trắc nghiệm",
+            "Tường thuật",
+        ],
+        colorMenu: "#5fae2e",
+        contentCate: {
+            first1page: {
+                img: "https://i1-thethao.vnecdn.net/2022/05/03/screenshot-2022-05-03-at-09-20-7164-4295-1651544469.png?w=380&h=228&q=100&dpr=1&fit=crop&s=rKlV3y5Ir_PJuGrfCODAaw",
+                title: "Ronaldo: 'Tôi chưa kết thúc'",
+                description:
+                    "ANH - Tiền đạo 37 tuổi ngụ ý sẽ ở lại Man Utd thêm một mùa giải, với phát biểu sau trận thắng Brentford ở Ngoại hạng Anh tối 2/5.  137",
+            },
+            newsbox: {
+                title: 'Nadal:"Alcaraz sẽ đoạt rất nhiều Grand Slam"',
+                description: `TÂY BAN NHA - Rafael Nadal cho rằng đàn em đồng hương Carlos Alcaraz sẽ lên số một thế giới với ... `,
+            },
+            footer: [
+                `CĐV Man Utd ra về phút 73`,
+                `Rangnick: 'Man Utd cần thay đổi diện rộng, trừ thủ môn' <div class="box-comment">
+                <i class="fas fa-comment-alt"></i>
+                <span>56</span>
+            </div>`,
+                `Klopp: 'Liverpool phải cố thắng trên sân Villarreal'`,
+            ],
+        },
+    },
+    "Pháp luật": { sub_nav: ["Hồ sơ phá án", "Tư vấn"], colorMenu: "#b52759" },
+    "Giáo dục": {
+        sub_nav: [
+            "Tin tức",
+            "Tuyển sinh",
+            "Chân dung",
+            "Du học",
+            "Học tiếng Anh",
+            "Trắc nghiệm",
+            "Giáo dục 4.0",
+        ],
+        colorMenu: "#eb7600",
+        contentCate: {
+            first1page: {
+                img: "https://i1-vnexpress.vnecdn.net/2022/04/29/Settop-Tuyen-sinh-lop-10.jpg?w=380&h=228&q=100&dpr=1&fit=crop&s=XRHmKTcJP6csPaMh4IUulQ",
+                title: "Quy mô thi vào lớp 10 THPT ở Hà Nội, TP HCM",
+                description:
+                    "Năm nay, Hà Nội có 129.000 học sinh tốt nghiệp THCS, cao hơn TP HCM 29.000 em. Cả hai thành phố đều tổ chức thi vào lớp 10 với ba môn Toán ... ",
+            },
+            newsbox: {
+                title: "ĐH Công nghệ Giao thông vận tải giảm chỉ tiêu từ thi tốt nghiệp",
+                description: `Với phương thức xét tuyển bằng điểm thi tốt nghiệp THPT 2022, Đại học Công nghệ Giao thông vận tải dành ... `,
+            },
+            footer: [
+                `Tương lai ngành Kỹ thuật vật liệu thế nào?`,
+                `Kiểm tra kỹ năng đọc hiểu tiếng Anh trong 15 phút`,
+                `Thắc mắc về các ngành học 'bình mới rượu cũ'`,
+            ],
+        },
+    },
+    "Sức khỏe": {
+        sub_nav: [
+            "Tin tức",
+            "Tư vấn",
+            "Dinh dưỡng",
+            "Khỏe đẹp",
+            "Đàn ông",
+            "Các bệnh",
+            "Vaccine",
+            "Di chứng Covid",
+            "Di truyền",
+        ],
+        colorMenu: "#049b93",
+        contentCate: {
+            first1page: {
+                img: "https://i1-suckhoe.vnecdn.net/2022/05/03/279300306-3215282948741063-406-9479-2815-1651572560.jpg?w=380&h=228&q=100&dpr=1&fit=crop&s=T3NgSelABmrGATybXxWHpw",
+                title: "Nguy kịch sau hút thuốc lào",
+                description:
+                    "PHÚ THỌ - Thanh niên 25 tuổi, sau khi hút điếu thuốc lào thì lên cơn co giật, nôn, toàn hân tím tái, mất ý thức, phải cấp cứu. ",
+            },
+            newsbox: {
+                title: "Lần đầu không ghi nhận ca tử vong do Covid sau 282 ngày",
+                description: `Bộ Y tế tối 3/5 công bố thêm 2.709 ca nhiễm tại 53 tỉnh, thành; lần đầu không ghi nhận ca tử vong kể từ ... `,
+            },
+            footer: [
+                `5 điều cần biết khi mắc sốt xuất huyết`,
+                `Ba trẻ Indonesia tử vong vì bệnh viêm gan bí ẩn`,
+                `Tẩy tế bào chết có hại da không?
+            `,
+            ],
+        },
+    },
+    "Đời sống": {
+        sub_nav: ["Tổ ấm", "Bài học sống", "Nhà", "Tiêu dùng", "Cooking"],
+        colorMenu: "#065e9d",
+        contentCate: {
+            first1page: {
+                img: "https://i1-giadinh.vnecdn.net/2022/04/28/Bc88-1651133633-7405-1651133929.jpg?w=380&h=228&q=100&dpr=1&fit=crop&s=7XVj4kV6vs_n7cmJa5BlCg",
+                title: "Bí quyết làm canh cá Quỳnh Côi chuẩn vị",
+                description:
+                    "Một bát canh cá dân dã, trọn hương - sắc - vị với thịt cá dai giòn, nước dùng thanh, trứng cá vàng ươm, rau xanh, bánh đa trắng... tựa bức ...",
+            },
+            newsbox: {
+                title: "Những loại củ quả không nên để gần nhau",
+                description: `Một số loại quả khi chín tạo ra ethylene, một loại khí làm thực phẩm khác chín sớm hơn hoặc thối rữa nhanh hơn. `,
+            },
+            footer: [
+                `Người mẫu kiểm tra IQ bạn trai trước khi hẹn hò`,
+                `Kết hôn ở độ cao 11.000 m`,
+                `Chàng Australia trúng 'sét ái tình' cô gái Việt <div class="box-comment">
+            <i class="fas fa-comment-alt"></i>
+            <span>17</span>
+        </div>`,
+            ],
+        },
+    },
+    "Du lịch": {
+        sub_nav: [
+            "Điểm đến",
+            "Ẩm thực",
+            "Dấu chân",
+            "Tư vấn",
+            "Cẩm nang",
+            "Ảnh",
+            "Ăn và chơi",
+            "Mở cửa du lịch",
+        ],
+        colorMenu: "#065e9d",
+    },
+    "Số hóa": {
+        sub_nav: [
+            "Công nghệ",
+            "Sản phẩm",
+            "Blockchain",
+            "Tech Awards 2022",
+            "CTO Summit 2022",
+            "Startup",
+        ],
+        colorMenu: "#b88000",
+    },
+    Xe: {
+        sub_nav: ["Thị trường", "Cầm lái", "Diễn đàn", "V-Car", "Thi bằng lái", "Mua bán"],
+        colorMenu: "#065e9d",
+    },
+    "Ý kiến": { sub_nav: ["Thời sự", "Đời sống"], colorMenu: "#b52759" },
+    "Tâm sự": {
+        sub_nav: ["Chuyên gia gỡ rối", "Hẹn hò"],
+        colorMenu: "#b52759",
+    },
+    Hài: { sub_nav: ["Cười", "Đố vui", "Chuyện lạ"], colorMenu: "#e7776a" },
+};
+const menuBox = document.querySelector(".menu");
+const allTitle = Object.keys(dataMenu);
+function renderingHeader() {
+    const menuDataHTML = [];
+    for (var value of allTitle) {
+        const MenuItemData = dataMenu[value];
+        const subMenuItem = [];
+        for (let i = 0; i < MenuItemData.sub_nav.length; i++) {
+            subMenuItem.push(`<li class="sub__menu-item">
+            <a href="#" class="sub__menu-link"
+                >${MenuItemData.sub_nav[i]}</a
+            >
+        </li>`);
+        }
+        menuDataHTML.push(`<li class="menu-item" style="--color-title: ${MenuItemData.colorMenu}">
+        <a href="#" class="menu-link">${value}</a>
+        <ul class="sub__menu">
+            ${subMenuItem.join("")}
+        </ul>
+    </li>`);
+    }
+    menuBox.innerHTML = menuDataHTML.join("");
+}
+renderingHeader();
+
+//renderingAllMenu
+const allMenuBoxLeft = document.querySelector(".all-menu-left .row");
+const allMenuBoxHTML = [];
+for (var value of allTitle) {
+    allMenuBoxHTML.push(` <div class="col c-4 md-2 lg-2 all-menu-left-item">
+    <h4
+    class="all-menu-left-item-title"
+>
+    ${value}
+    </h4>
+    <ul class="all-menu-list">
+    ${dataMenu[value].sub_nav
+        .reduce((acc, curr, index) => {
+            return [
+                ...acc,
+                ` <li class="all-menu-item ${index > 3 ? (style = "display:none;") : ""}">
+        <a
+            href="#"
+            class="all-menu-link"
+            >${curr}</a
+        >
+    </li>`,
+            ];
+        }, [])
+        .join("")}
+        ${dataMenu[value].sub_nav.length > 4 ? '<li class="btn-more">Xem thêm</li>' : ""}<ul>
+
+</div>`);
+}
+allMenuBoxLeft.innerHTML = allMenuBoxHTML.join("");
+
+// Rendering Content-Right
+const contentRight = document.querySelector(".content-right");
+const contentRightArray = [
+    "Kinh doanh",
+    "Thể thao",
+    "Giải trí",
+    "Sức khỏe",
+    "Đời sống",
+    "Khoa học",
+    "Giáo dục",
+];
+for (let i = 0; i < contentRightArray.length; i++) {
+    const mainCate = contentRightArray[i];
+    const dataContent = dataMenu[contentRightArray[i]];
+    const contentCateData = dataContent.contentCate;
+    const divCate = ` <div
+    class="content-category grid"
+    style="--main-color: ${dataContent.colorMenu}"
+>
+    <ul class="row content-category-header">
+        <li class="main-cate">
+            <a href="#">${mainCate}</a>
+        </li>
+        ${dataContent.sub_nav
+            .reduce((acc, curr, index) => {
+                return index > 5
+                    ? [...acc]
+                    : [
+                          ...acc,
+                          `<li class="sub-cate">
+            <a href="#">${curr}</a>
+        </li>`,
+                      ];
+            }, [])
+            .join("")}
+        
+     
+    </ul>
+    <div class="row content-category-body bdbt">
+        <div class="first1page col c-12 md-8">
+            <img
+                src="${contentCateData.first1page.img}"
+                alt=""
+            />
+            <div class="news-box">
+                <a href="#" class="news-link">
+                    <h3 class="news-title">
+                    ${contentCateData.first1page.title}
+                    </h3>
+                    <p class="news-description">
+                    ${contentCateData.first1page.description}
+                    </p>
+                </a>
+            </div>
+        </div>
+        <div class="news-box bdleft col c-12 md-4">
+            <h3 class="news-title">
+            ${contentCateData.newsbox.title}
+            </h3>
+            <p class="news-description">
+            ${contentCateData.newsbox.description}
+            </p>
+        </div>
+    </div>
+    <ul class="row content-category-footer">
+        <li class="col c-12 md-4">
+            <a href="#" class="link"
+                >${contentCateData.footer[0]}</a
+            >
+        </li>
+        <li class="col c-12 md-4">
+            <a href="#" class="link"
+                >${contentCateData.footer[1]}</a
+            >
+        </li>
+        <li class="col c-12 md-4">
+            <a href="#" class="link"
+                >${contentCateData.footer[2]}</a
+            >
+        </li>
+    </ul>
+</div>`;
+    contentRight.innerHTML += divCate;
+}
+
+// Event \\
+const btnMores = document.querySelectorAll(".btn-more");
+btnMores.forEach((btnMore) => {
+    btnMore.addEventListener("click", () => {
+        btnMore.parentElement.classList.add("active");
+    });
+});
+//Event Allmenu
+const btnAll = document.querySelector(".btn-all");
+const btnToggle = document.querySelector(".btn-toggle");
+const btnCloseAllMenu = document.querySelector(".btn-close-all-menu");
+const allMenuBox = document.querySelector(".all-menu-section");
+btnAll.addEventListener("click", () => {
+    document.body.classList.toggle("all-menu-active");
+});
+btnCloseAllMenu.addEventListener("click", () => {
+    document.body.classList.remove("all-menu-active");
+});
+btnToggle.addEventListener("click", () => {
+    document.body.classList.toggle("all-menu-active");
+});
+// Event Nav
+window.addEventListener("scroll", (e) => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        console.log("a");
+
+        document.body.classList.add("sticky");
+    } else {
+        document.body.classList.remove("sticky");
+    }
+});
+const newsBoxPrevs = document.querySelectorAll(
+    ".section-picture .swiper-slide .news-box:nth-child(2)"
+);
+const btnNextPictureBox = document.querySelector(".section-picture .swiper .swiper-button-next ");
+newsBoxPrevs.forEach((newsBoxPrev) => {
+    newsBoxPrev.addEventListener("click", () => {
+        btnNextPictureBox.click();
+    });
+});
+//Modal
+const modalContainer = document.querySelector(".modal-container");
+const signupBtn = document.querySelector(".signup-btn");
+const signinBtn = document.querySelector(".signin-btn");
+signupBtn.addEventListener("click", () => {
+    modalContainer.classList.add("signup");
+});
+signinBtn.addEventListener("click", () => {
+    modalContainer.classList.remove("signup");
+});
+const modalBtn = document.querySelector(".header__login");
+modalBtn.addEventListener("click", () => {
+    document.body.classList.add("auth-active");
+});
+const modalAuth = document.querySelector(".modal-auth");
+modalAuth.addEventListener("click", (e) => {
+    e.target.closest(".modal-container") ? true : document.body.classList.remove("auth-active");
+});
